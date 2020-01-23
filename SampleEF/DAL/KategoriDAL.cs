ddadad397d0fe5bbb78f581d344f135fdcab8b5a
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-01-23 11:21:58 +0700

    Add project files.

---
+++ SampleEF/DAL/KategoriDAL.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SampleEF.Data;
+
+namespace SampleEF.DAL
+{
+    public class KategoriDAL
+    {
+        private SampleDbEntities _db;
+        public KategoriDAL()
+        {
+            _db = new SampleDbEntities();
+        }
+
+        public IEnumerable<Kategori> GetAll()
+        {
+            //var results = _db.Kategori.OrderBy(k=>k.nama_kat);
+            var results = from k in _db.Kategori
+                          orderby k.nama_kat ascending
+                          select k;
+            return results;
+        }
+
+        public Kategori GetById(int id_kat)
+        {
+            //var result = _db.Kategori.Where(k => k.id_kat == id_kat).SingleOrDefault();
+            var result = (from k in _db.Kategori
+                          where k.id_kat == id_kat
+                          select k).SingleOrDefault();
+            return result;
+        }
+
+        public void Insert(string nama_kat)
+        {
+            var newKat = new Kategori
+            {
+                nama_kat = nama_kat
+            };
+            try
+            {
+                _db.Kategori.Add(newKat);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Update(int id_kat,string nama_kat)
+        {
+            var kategori = _db.Kategori.Where(k => k.id_kat == id_kat)
+                .SingleOrDefault();
+
+            if (kategori != null)
+            {
+                try
+                {
+                    kategori.nama_kat = nama_kat;
+                    _db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+            else
+            {
+                throw new Exception("Data tidak ditemukan");
+            }
+        }
+    }
+}
---
+++ SampleEF/KategoriPage.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="KategoriPage.aspx.cs" Inherits="SampleEF.KategoriPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:ObjectDataSource ID="odsKategori" TypeName="SampleEF.DAL.KategoriDAL"
+        SelectMethod="GetAll"  InsertMethod="Insert" runat="server" />
+    <div class="row">
+        <div class="col-md-6">
+            <asp:GridView ID="gvKategori" CssClass="table table-bordered"
+                DataSourceID="odsKategori" runat="server">
+            </asp:GridView>
+        </div>
+        <div class="col-md-6">
+            <asp:FormView ID="frmKat" DefaultMode="Insert" DataSourceID="odsKategori" runat="server">
+                <InsertItemTemplate>
+                    <div class="form-group">
+                        <label>Nama Kategori :</label>
+                        <asp:TextBox ID="txtNamaKategori" Text='<%# Bind("nama_kat") %>' CssClass="form-control" 
+                            runat="server" />
+                    </div>
+                    <asp:Button ID="btnSubmit" CssClass="btn btn-success btn-sm" 
+                        Text="Submit" CommandName="Insert" runat="server" />
+                </InsertItemTemplate>
+            </asp:FormView>
+        </div>
+    </div>
+
+</asp:Content>
